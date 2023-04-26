0eb16c64e10aefb0940cfe4248eb2b18d0280c5a
Christopher Ramroop <78105128+KillerKroc23@users.noreply.github.com>
2023-04-26 15:41:18 -0400

    First Level
    First Level Finished, just need to add the prize at the end. Eric have fun making the enemies. I left some gaps so you can place the enemies there. Get creative with it i trust you

--- Assets/Moving.cs
+++ Assets/Moving.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Move : MonoBehaviour
+public class Moving : MonoBehaviour
 {
     Animator ani;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,28 +15,38 @@ public class Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ani.speed = 4;
+        ani.speed = 1;
         float val_x = Input.GetAxis("Horizontal");
 
+    
+        // if val_x is less than 0, then set direction to 3
         if (val_x > 0)
         {
             ani.SetInteger("Direction", (int)MoveDirection.RIGHT);
         }
-
+        else if (val_x < 1)
+        {
+            ani.SetInteger("Direction", (int)MoveDirection.LEFT);
+        }
         else
         {
             ani.speed = 0;
         }
 
         transform.position += new Vector3(val_x, 0).normalized * Time.deltaTime;
-    }
 
+        // do this for all the directions
+        // Then move the guy just like you did for animation 1.
+
+
+    }
 
     public enum MoveDirection
     {
-
         NONE = 0,
-        RIGHT = 4,
+        LEFT = 1,
+        RIGHT = 2,
     }
+
 
 }
